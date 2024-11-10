3b7fdf80e67856e0a9401b56b21fb24b0b5a6316
Patrik Huzina <patrikhuzina@gmail.com>
2024-11-10 01:24:16 +0100

    func: Answer functionality added

--- src/components/QuestionSelector.jsx
+++ src/components/QuestionSelector.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faArrowAltCircleDown,
+  faArrowAltCircleRight,
+  faArrowRight,
   faFileExport,
   faFileImport,
   faPencil,
@@ -16,6 +19,8 @@ const QuestionSelector = ({ selectedTopic }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [isAnswerPopupOpen, setIsAnswerPopupOpen] = useState(false);
 
   useEffect(() => {
     const savedQuestions = localStorage.getItem("questions" + selectedTopic);
@@ -85,6 +90,25 @@ const QuestionSelector = ({ selectedTopic }) => {
     setIsDeletePopupOpen(false);
   };
 
+  const createNewAnswer = (questionIndex) => {
+    const answerWithId = {
+      text: "Változasd meg.",
+      correct: false,
+    };
+
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].answers.push(answerWithId);
+    setQuestions(updatedQuestions);
+  };
+
+  const editAnswer = (questionIndex, answerIndex) => {
+    setIsAnswerPopupOpen(true);
+    setSelectedAnswer({
+      questionIndex,
+      answerIndex,
+    });
+  };
+
   return (
     <div className="w-full h-full">
       {selectedTopic ? (
@@ -115,25 +139,98 @@ const QuestionSelector = ({ selectedTopic }) => {
             className="w-full h-full p-4 flex flex-col"
             style={{ height: "92.5%" }}>
             <div className="flex-auto w-full overflow-auto gap-2 flex flex-col max-h-fit">
-              {questions.map((question, index) => (
+              {questions.map((question, questionIndex) => (
                 <div
                   key={question.id}
-                  className="w-full px-4 py-2 min-h-fit flex items-center justify-start px-4 border border-border bg-main text-white font-roboto">
-                  {question.text}
-                  <FontAwesomeIcon
-                    icon={faPencil}
-                    className="ml-2 text-white opacity-20 cursor-pointer transition-all duration-200 hover:text-blue hover:opacity-100"
-                    onClick={() => editQuestion(question.id)}
-                  />
+                  className="w-full px-4 py-2 min-h-fit flex flex-col border border-border bg-main text-white font-roboto">
+                  <div className="w-full h-fit flex items-center justify-start">
+                    <FontAwesomeIcon
+                      icon={faArrowAltCircleRight}
+                      className={`mr-2 cursor-pointer ${
+                        question.opened
+                          ? "rotate-90 text-orange opacity-100"
+                          : "text-white opacity-20 rotate-0"
+                      } transition-all duration-200 hover:text-orange hover:opacity-100 hover:rotate-90`}
+                      onClick={() => {
+                        const updatedQuestions = [...questions];
+                        updatedQuestions[questionIndex].opened =
+                          !question.opened;
+                        setQuestions(updatedQuestions);
+                      }}
+                    />
 
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    className="ml-auto text-white opacity-20 cursor-pointer transition-all duration-200 hover:text-red hover:opacity-100"
-                    onClick={() => {
-                      setIsDeletePopupOpen(true);
-                      setSelectedQuestion(question.id);
-                    }}
-                  />
+                    {question.text}
+
+                    <FontAwesomeIcon
+                      icon={faPencil}
+                      className="ml-2 text-white opacity-20 cursor-pointer transition-all duration-200 hover:text-blue hover:opacity-100"
+                      onClick={() => editQuestion(question.id)}
+                    />
+
+                    <FontAwesomeIcon
+                      icon={faXmark}
+                      className="ml-auto text-white opacity-20 cursor-pointer transition-all duration-200 hover:text-red hover:opacity-100"
+                      onClick={() => {
+                        setIsDeletePopupOpen(true);
+                        setSelectedQuestion(question.id);
+                      }}
+                    />
+                  </div>
+
+                  {question.opened && (
+                    <div className="w-full px-4 py-2 mt-2 min-h-fit flex flex-col items-start border border-border bg-main text-white font-roboto gap-2">
+                      {question.answers.map((answer, answerIndex) => (
+                        <div
+                          key={answerIndex}
+                          className="w-full px-4 py-2 min-h-fit flex items-center justify-start border border-border bg-main text-white font-roboto">
+                          <input
+                            type="checkbox"
+                            className="mr-2"
+                            checked={answer.correct}
+                            onChange={(e) => {
+                              const updatedQuestions = [...questions];
+                              updatedQuestions[questionIndex].answers[
+                                answerIndex
+                              ].correct = e.target.checked;
+                              setQuestions(updatedQuestions);
+                            }}
+                          />
+                          {answer.text}
+                          {answer.correct && (
+                            <span class="ml-2 text-green">(Helyes)</span>
+                          )}
+
+                          <FontAwesomeIcon
+                            icon={faPencil}
+                            className="ml-2 text-white opacity-20 cursor-pointer transition-all duration-200 hover:text-blue hover:opacity-100"
+                            onClick={() =>
+                              editAnswer(questionIndex, answerIndex)
+                            }
+                          />
+
+                          <FontAwesomeIcon
+                            icon={faXmark}
+                            className="ml-auto text-white opacity-20 cursor-pointer transition-all duration-200 hover:text-red hover:opacity-100"
+                            onClick={() => {
+                              const updatedQuestions = [...questions];
+                              updatedQuestions[questionIndex].answers.splice(
+                                answerIndex,
+                                1
+                              );
+                              setQuestions(updatedQuestions);
+                            }}
+                          />
+                        </div>
+                      ))}
+
+                      <div
+                        className="w-full px-4 py-2 min-h-fit flex items-center text-sm justify-center border border-border bg-green text-white font-roboto transition-all duration-200 hover:bg-green-hover cursor-pointer hover:text-black"
+                        onClick={() => createNewAnswer(questionIndex)}>
+                        <FontAwesomeIcon icon={faPlus} className="mr-2" />
+                        Új válasz hozzáadása
+                      </div>
+                    </div>
+                  )}
                 </div>
               ))}
             </div>
@@ -183,6 +280,30 @@ const QuestionSelector = ({ selectedTopic }) => {
             }
           }}
           onClose={() => setIsPopupOpen(false)}
+        />
+      )}
+
+      {isAnswerPopupOpen && (
+        <NewTopicPopup
+          header="Válasz szerkesztése"
+          placeholder="Válasz szövege"
+          defaultName={
+            questions[selectedAnswer.questionIndex].answers[
+              selectedAnswer.answerIndex
+            ].text
+          }
+          input={true}
+          button="Mentés"
+          onSave={(newAnswer) => {
+            const updatedQuestions = [...questions];
+            updatedQuestions[selectedAnswer.questionIndex].answers[
+              selectedAnswer.answerIndex
+            ].text = newAnswer;
+
+            setQuestions(updatedQuestions);
+            setIsAnswerPopupOpen(false);
+          }}
+          onClose={() => setIsAnswerPopupOpen(false)}
         />
       )}
 
